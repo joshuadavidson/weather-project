e2b2275b825301ffce83415e365c2fa276aa8fe8
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-05-11 18:35:55 -0500

    Completed project.

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,210 @@
-var userLoc = {
-  //Set Default to Antarctica
-  lat: 82.8628,
-  lon: 135.0000
+var userData = {
+  position: {
+    lat: 0,
+    lon: 0
+  },
+  location: {
+    city: "",
+    state: "",
+    country: "",
+    locationText: ""
+  },
+  weather: {
+    timeStampWeather: 0, //epoch time in ms
+    summary: "", //short description of weather
+    humidity: 0, //0-1
+    precipProb: 0, //0-1
+    icon: "", //icon to display
+    imperial: { //data in imperial units
+      temp: 0, //Fahrenheit
+      tempUnits: "&degF",
+      windSpeed: 0, //mph
+      windSpeedUnits: "mph"
+    },
+    metric: { //data in metric units
+      temp: 0, //Celsius
+      tempUnits: "&degC",
+      windSpeed: 0, //kph
+      windSpeedUnits: "kph"
+    }
+  }
 };
-getUserLocation.
-function getUserLocation() {
-  console.log("Inside getUserLocation function")
-    //If browser has location enabled update userLoc
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      userLoc.lat = position.coords.latitude;
-      userLoc.lon = position.coords.longitude;
-      return;
-    });
-  } else { //If browser doesn't have location enabled get from IP address
-    $.getJSON("http://freegeoip.net/json/", function(json) {
-      console.log("Pulling from IP.");
-      userLoc.lat = json.latitude;
-      userLoc.lon = json.longitude;
+
+var videoTagLookup = {
+  "clear-day": '<video poster="./img/clear-day.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/clear-day.mp4" type="video/mp4"></video>',
+  "clear-night": '<video poster="./img/clear-night.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/clear-night.mp4" type="video/mp4"></video>',
+  "rain": '<video poster="./img/rain.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/rain.mp4" type="video/mp4"></video>',
+  "snow": '<video poster="./img/snow.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/snow.mp4" type="video/mp4"></video>',
+  "sleet": '<video poster="./img/snow.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/snow.mp4" type="video/mp4"></video>',
+  "wind": '<video poster="./img/wind.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/wind.mp4" type="video/mp4"></video>',
+  "fog": '<video poster="./img/fog.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/fog.mp4" type="video/mp4"></video>',
+  "cloudy": '<video poster="./img/cloudy.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/cloudy.mp4" type="video/mp4"></video>',
+  "partly-cloudy-day": '<video poster="./img/partly-cloudy-day.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/partly-cloudy-day.mp4" type="video/mp4"></video>',
+  "partly-cloudy-night": '<video poster="./img/partly-cloudy-night.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/partly-cloudy-night.mp4" type="video/mp4"></video>',
+  "hail": '<video poster="./img/thunderstorm.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/thunderstorm.mp4" type="video/mp4"></video>',
+  "thunderstorm": '<video poster="./img/thunderstorm.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/thunderstorm.mp4" type="video/mp4"></video>',
+  "tornado": '<video poster="./img/thunderstorm.jpg" preload="auto" autoplay="true" loop="loop" muted="muted"><source id="bgVideo" src="./img/thunderstorm.mp4" type="video/mp4"></video>'
+}
+
+var iconLookup = {
+  "clear-day": "wi-forecast-io-clear-day",
+  "clear-night": "wi-forecast-io-clear-night",
+  "rain": "wi-forecast-io-rain",
+  "snow": "wi-forecast-io-snow",
+  "sleet": "wi-forecast-io-sleet",
+  "wind": "wi-forecast-io-wind",
+  "fog": "wi-forecast-io-fog",
+  "cloudy": "wi-forecast-io-cloudy",
+  "partly-cloudy-day": "wi-forecast-io-partly-cloudy-day",
+  "partly-cloudy-night": "wi-forecast-io-partly-cloudy-night",
+  "hail": "wi-forecast-io-hail",
+  "thunderstorm": "wi-forecast-io-thunderstorm",
+  "tornado": "wi-forecast-io-tornado"
+}
+
+function getUserPosition() {
+
+  //get position from browser
+  function posFromBrowser(position) {
+    userData.position.lat = position.coords.latitude;
+    userData.position.lon = position.coords.longitude;
+    getUserLocation();
+  };
+
+  //get position via IP
+  function posFromIP() {
+    $.ajax({
+      url: "https://freegeoip.net/json/",
+      dataType: 'json',
+      async: true,
+      success: function(ipLoc) {
+        userData.position.lat = ipLoc.latitude;
+        userData.position.lon = ipLoc.longitude;
+        getUserLocation();
+      },
+      error: function() {
+        alert("Unable to determine location.");
+      }
     });
   }
-  console.log("End of funtion" + userLoc.lat);
+
+  //check if geolocation is enabled
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(posFromBrowser, posFromIP); //try browser first then attempt IP
+  } else { //if geolocation not available just use IP
+    posFromIP();
+  }
+}
+
+//find user location with Reverse Geocode from google
+function getUserLocation() {
+  let assembledURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + userData.position.lat + "," + userData.position.lon + "&key=AIzaSyCP8GLrC7_6nQYpBJFfTn4YWfz8G42y80g"
+  $.ajax({
+    url: assembledURL,
+    dataType: 'json',
+    async: true,
+    success: function(json) {
+      //find the city, state, and country from google geocode results
+      for (let i = 0; i < json.results[0].address_components.length; i++) {
+        for (let j = 0; j < json.results[0].address_components[i].types.length; j++) {
+          var currType = json.results[0].address_components[i].types[j];
+          switch (currType) {
+            case "country":
+              userData.location.country = json.results[0].address_components[i].short_name;
+              break;
+            case "administrative_area_level_1":
+              userData.location.state = json.results[0].address_components[i].short_name;
+              break;
+            case "locality":
+              userData.location.city = json.results[0].address_components[i].short_name;
+              break;
+          }
+        }
+      }
+      //Assemble the location text and remove empty values placing ',' between them
+      userData.location.locationText = userData.location.city + "\u0009" + userData.location.state + "\u0009" + userData.location.country;
+      userData.location.locationText = userData.location.locationText.trim().replace(/\t/g, ", ");
+      getUserWeather();
+    },
+    error: function() {
+      alert("City, State, Country data could not be found.");
+      getUserWeather();
+    }
+  });
+}
+
+function getUserWeather() {
+  let assembledURL = "https://api.forecast.io/forecast/5d462c19218fb1f2697e53fefda9aac7/" + userData.position.lat + "," + userData.position.lon;
+  $.ajax({
+    url: assembledURL,
+    dataType: 'jsonp',
+    async: true,
+    success: function(json) {
+      userData.weather.timeStampWeather = json.currently.time;
+      userData.weather.summary = json.currently.summary;
+      userData.weather.humidity = json.currently.humidity;
+      userData.weather.precipProb = json.currently.precipProbability;
+      userData.weather.icon = json.currently.icon;
+
+      userData.weather.imperial.temp = json.currently.temperature;
+      userData.weather.imperial.windSpeed = json.currently.windSpeed;
+      getMetricUserData(); //Convert imperial data to metric
+      printUserData("imperial");
+    },
+    error: function() {
+      alert("Unable to fetch weather from forecast.io.")
+    }
+  });
+}
+
+function getMetricUserData() {
+  userData.weather.metric.temp = (userData.weather.imperial.temp - 32) * 5 / 9;
+  userData.weather.metric.windSpeed = userData.weather.imperial.windSpeed * 1.609344;
+}
+
+
+function printUserData(unitSystem) {
+  //Fade all the data in at once
+  $("#weatherBlock").fadeIn(1000);
+  $("#creditsBlock").fadeIn(1000);
+  $(".background").fadeIn(1000);
+  //Update all data while fade is working
+  $("#locationText").html(userData.location.locationText);
+  $("#summary").html(userData.weather.summary);
+  $("#temp").html(Math.round(10 * userData.weather[unitSystem].temp) / 10);
+  $("#humidity").html(Math.round(100 * userData.weather.humidity) + "%");
+  $("#precipProb").html(Math.round(100 * userData.weather.precipProb) + "%");
+  $("#windSpeed").html(Math.round(10 * userData.weather[unitSystem].windSpeed) / 10 + " " + userData.weather[unitSystem].windSpeedUnits);
+  $("#weatherIcon").removeClass().addClass(function() {
+    return "wi " + iconLookup[userData.weather.icon] + " fa-5x";
+  });
+  //update the background by looking at the weather icon only if it is not present
+  //this if check prevents reload of background if only the units were changed
+  if ($(".background").children("video").length === 0) {
+    $(".background").html(videoTagLookup[userData.weather.icon]);
+  }
 }
 
 $(document).ready(function() {
 
-  getUserLocation();
+  //start by getting user position
+  getUserPosition();
 
-  $("#latnum").html(userLoc.lat);
-  $("#lonnum").html(userLoc.lon);
+  //toggle from C to F
+  $("#degF").click(function() {
+    if ($("#degC").hasClass("selected")) {
+      $("#degC").removeClass();
+      $("#degF").removeClass().addClass("selected");
+      printUserData("imperial");
+    }
+  });
 
+  //toggle from F to C
+  $("#degC").click(function() {
+    if ($("#degF").hasClass("selected")) {
+      $("#degF").removeClass();
+      $("#degC").removeClass().addClass("selected");
+      printUserData("metric");
+    }
+  });
 });
