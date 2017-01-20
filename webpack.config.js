b0c63113f09459a0f16a27e92c22f98f806b09b9
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2017-01-20 15:27:43 -0600

    Updates UglifyJsPlugin mangle to true.

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      mangle: false,
-      sourcemap: true,
+      mangle: true,
+      sourcemap: false,
     }),
   ],
 };
